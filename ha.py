d85492db68e4a8f33d9652b126076a4766d574a0
python-harshita <48007832+python-harshita@users.noreply.github.com>
2019-02-28 15:27:30 +0530

    Add files via upload

---
+++ ha.py
@@ -0,0 +1,10 @@
+import random
+
+	while True
+	d=input("press r to ,q to quit:")
+
+if d=='r':
+		print ("you got:"random.randint(1,6))
+	elif d=='q':
+		print("bye!")
+		exit()
---
+++ put.py
@@ -0,0 +1,11 @@
+num1=3
+num2=4
+num3=5
+if (num1>=num2) and (num2>=num3):
+		largest=num1
+elif (num2>=num1) and (num2>=num3):
+	largest=num2
+else:
+	largest=num3
+
+print("the largest number between",num1,",",num2,"and",num3,"is",largest)		
